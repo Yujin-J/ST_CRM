f3d12ce4c6328e5e134ea8f5947b1815b8202c8d
김도헌 <ehgjs6569@gmail.com>
2025-01-13 11:13:29 +0700

    Implemented main page's count
    Not graph, though.
    
    ** Seperated firebase config into refine version and base version(As in firebaseConfig). It is quite spagetti, but... currently it is essential because each service requires different version of firebase...

--- src/helpers/firebase/firebaseConfig.ts
+++ src/helpers/firebase/firebaseConfig.ts
@@ -6,6 +6,7 @@ import {
 
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
 
 export const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -23,6 +24,9 @@ export const auth = getAuth(firebaseApp);
 export const googleProvider = new GoogleAuthProvider();
 export const firestoreDatabase = getFirestore(firebaseApp);
 
+// Spagetti mechanism... But for working and compatibility
+export const firebaseApp_base = initializeApp(firebaseConfig);
+export const firestoreDatabase_base = getFirestore(firebaseApp_base);
 // Export Firestore object as "db" for consistency
 export const firebaseAuth = new FirebaseAuth();
 export const firestoreDataProvider = new FirestoreDatabase();
--- src/helpers/firebase/firebaseService.ts
+++ src/helpers/firebase/firebaseService.ts
@@ -1,8 +1,5 @@
-import { firebaseApp } from "./firebaseConfig"; // Firebase 앱 가져오기
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-
-// Firestore 기본 인스턴스 생성 (refine-firebase와 별도 사용)
-const firestore = getFirestore(firebaseApp);
+import { firestoreDatabase_base } from "./firebaseConfig"; // Firestore 인스턴스 가져오기
+import { collection, getDocs } from "firebase/firestore";
 
 /**
  * Firestore 컬렉션의 문서 개수를 반환하는 함수
@@ -11,16 +8,11 @@ const firestore = getFirestore(firebaseApp);
  */
 export const fetchCollectionCount = async (collectionName: string): Promise<number> => {
   try {
-    // Firestore에서 컬렉션 참조 생성
-    console.log("Fetching collection count for:", collectionName);
-
-    const collectionRef = collection(firestore, collectionName); // 기본 Firestore 인스턴스 사용
+    const collectionRef = collection(firestoreDatabase_base, collectionName); // Firestore 인스턴스 사용
     const querySnapshot = await getDocs(collectionRef);
-    console.log("Document count:", querySnapshot.size);
-
     return querySnapshot.size; // 문서 개수 반환
   } catch (error) {
     console.error(`Error fetching collection count for ${collectionName}:`, error);
-    throw error; // 에러 다시 던지기
+    throw error;
   }
 };
